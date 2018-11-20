54066e70a594a71d570ed0e15e548b772ad2c4a8
unknown <js33063@gmail.com>
2018-11-20 14:20:13 +0530

    'update'

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-var express = require('express');
+var express = require("express");
 
 var router = express.Router();
 
 // localhost:3000/aaa
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('case', { msg: "Welcome" });
+router.get("/", function(req, res, next) {
+    res.render("case", { msg: "Welcome" });
 });
 
-router.get('/casesubmit', function(req, res, next) {
-    res.render('casesubmit', { msg: "Welcome" });
+router.get("/casesubmit", function(req, res, next) {
+    res.render("casesubmit", { msg: "Welcome" });
 });
 
-
-
-
+router.get("/live", function(req, res, next) {
+    res.render("live", { msg: "Welcome" });
+});
 
 module.exports = router;
